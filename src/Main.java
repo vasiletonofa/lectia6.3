fa90f69ee014a09c9085ad97edb45c1b1250369a
vasile <vasile.tonofa@gmail.com>
2023-10-06 20:29:53 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,68 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+//        int a = 4;
+//        int b = 8;
+//        int c = 6;
+//        int d = 2;
+//        int e = 7;
+//        int t = 1;
+//
+//
+//        for (int i = 0; i < 10; i++) {
+//            if(a == 9) {
+//
+//            }
+//
+//            if(b == 9) {
+//
+//            }
+//
+//
+//            if(c == 9) {
+//
+//            }
+//
+//            if(d == 9) {
+//
+//            }
+//        }
+
+
+//        Integer[] numere = new Integer[10];
+//
+//        numere[0] = 4;
+//        numere[1] = 8;
+//        numere[2] = 6;
+//        numere[3] = 2;
+//        numere[4] = 5;
+//        numere[5] = null;
+//        numere[9] = 4;
+//
+//
+//        for (int i = 0; i < numere.length; i++) {
+//            Integer a = numere[i]; //  numere[5] -> null.intValue()
+//            if (a != null && a == 2) {
+//                System.out.println("Am gasit 2");
+//            } else {
+//                System.out.println(a);
+//            }
+//
+//        }
+
+
+        String[] str = new String[5];
+        str[0] = "Andrei";
+        str[1] = "Ion";
+        str[2] = "Vasile";
+
+
+        for (int i = 0; i < str.length; i++) {
+            System.out.println(str[i]);
+        }
+
+
+
+    }
+}
